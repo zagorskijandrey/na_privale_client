304c6e66b7440d9c5cf6cdd2b33f1d1d0bc8846f
azagorskyi <andrii.zagorskyi@onseo.biz>
2017-08-19 19:11:41 +0300

    typewriter method

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -7,30 +7,40 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private typewriter_texts: String[] = ['Женщина, которая никогда не видела своего мужа за ужением рыбы, не имеет понятия' +
+    ', за какого терпеливого человека она вышла замуж - Эдгар Хау',
+    'Самая плохая рыбалка лучше самой хорошей работы.',
+    'Рыболовы бывают двух видов: одни смотрят на это занятие как на спорт, другим удается что-то поймать.'];
+  private typewriter_text: String = 'Рыболовы бывают двух видов';
+  private typewriter_display: String = '';
+  private count: Number = 0;
+
   constructor() {
   }
 
   ngOnInit() {
-    this.typeWriter();
+    // for (let i = 0; i < this.typewriter_texts.length; i++) {
+    //   this.typingCallback(this, this.typewriter_texts[i], this.typewriter_display);
+    // }
+    // this.typingCallback(this);
+    this.typingCallback(this);
+
   }
 
-  typeWriter() {
-    // var text = $('.sub-elements').text();
-
-    // var length = text.length;
-    var timeOut;
-    var character = 0;
-
-    // timeOut = setTimeout(function () {
-    //   character++;
-    //   var type = text.substring(0, character);
-    // //   $('.sub-elements').text(type);
-    // //   typeWriter();
-    // //
-    // //   if (character == length) {
-    // //     clearTimeout(timeOut);
-    // //   }
-    // //
-    // }, 150);
+  typingCallback(that) {
+    const total_length = that.typewriter_texts[that.count].length;
+    that.typewriter_text = that.typewriter_texts[that.count];
+    const current_length = that.typewriter_display.length;
+    let count = that.count;
+    if (current_length < total_length) {
+      that.typewriter_display += that.typewriter_text[current_length];
+    } else {
+      that.typewriter_display = '';
+      that.count = ++count;
+      if (that.count < that.typewriter_texts.length) {
+        that.typingCallback(that);
+      }
+    }
+    setTimeout(that.typingCallback, 100, that);
   }
 }

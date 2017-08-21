59e7b914057646f1d3002e899b69096ebc87858b
zagorskijandrey <zagorskij.andrey@gmail.com>
2017-08-21 22:50:03 +0300

    home component correct

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -6,41 +6,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  private typewriter_texts: String[] = ['Женщина, которая никогда не видела своего мужа за ужением рыбы, не имеет понятия' +
-    ', за какого терпеливого человека она вышла замуж - Эдгар Хау',
-    'Самая плохая рыбалка лучше самой хорошей работы.',
-    'Рыболовы бывают двух видов: одни смотрят на это занятие как на спорт, другим удается что-то поймать.'];
-  private typewriter_text: String = 'Рыболовы бывают двух видов';
-  private typewriter_display: String = '';
-  private count: Number = 0;
+  private typewriter_text:String = 'Женщина, которая никогда не видела своего мужа за ужением рыбы, не имеет понятия' +
+    ', за какого терпеливого человека она вышла замуж - Эдгар Хау;Самая плохая рыбалка лучше самой хорошей работы.;' +
+    'Рыболовы бывают двух видов: одни смотрят на это занятие как на спорт, другим удается что-то поймать.;' +
+    'Пока не поймаешь настоящую рыбу, рыбалка скучна, а после этого — отвратительна - Дейв Барри;' +
+    'Хороший клев бывает либо до того, как вы начали ловить, либо после этого.;' +
+    'Самыми крупными из пойманных рыб всегда бывают те, что сорвались с крючка - Юджин Филд;' +
+    'Одни рыболовы ловят рыбу, другие всю жизнь ее только подкармливают.;' +
+    'Чем длиннее руки у рыболова, тем меньше веры его рассказам.;' +
+    'Рыбалка — промежуточное звено между спортом и религией - Джозефин Тей;';
+  private typewriter_display:String = '';
+  private count:Number = 0;
 
   constructor() {
   }
 
   ngOnInit() {
-    // for (let i = 0; i < this.typewriter_texts.length; i++) {
-    //   this.typingCallback(this, this.typewriter_texts[i], this.typewriter_display);
-    // }
-    // this.typingCallback(this);
-    // this.typingCallback(this);
-
+    this.typingCallback(this);
   }
 
-  typingCallback(that) {
-    const total_length = that.typewriter_texts[that.count].length;
-    that.typewriter_text = that.typewriter_texts[that.count];
-    const current_length = that.typewriter_display.length;
-    let count = that.count;
-    if (current_length < total_length) {
-      that.typewriter_display += that.typewriter_text[current_length];
-    } else {
-      that.typewriter_display = '';
-      that.count = ++count;
-      if (that.count < that.typewriter_texts.length) {
-        that.typingCallback(that);
+    typingCallback(that)
+    {
+      const total_length = that.typewriter_text.length;
+      if (that.count < total_length) {
+        if(that.typewriter_text[that.count] == ';'){
+          that.typewriter_display = '';
+          ++that.count
+        } else {
+          that.typewriter_display += that.typewriter_text[that.count];
+          ++that.count;
+        }
+      } else {
+        that.typewriter_display = '';
+        that.count=0;
       }
+      setTimeout(that.typingCallback, 200, that);
     }
-    setTimeout(that.typingCallback, 100, that);
-  }
 }
